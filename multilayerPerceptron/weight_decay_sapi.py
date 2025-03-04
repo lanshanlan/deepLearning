cceca68a7f9a34412739b208e3a5ec9ff6b7b0c1
lanshanlan <2931735037@qq.com>
2025-03-04 22:14:56 +0800

    feat: 权重衰减简洁形式

--- multilayerPerceptron/weight_decay_sapi.py
+++ multilayerPerceptron/weight_decay_sapi.py
@@ -13,31 +13,28 @@ train_iter = load_array(train_data, batch_size)
 test_data = synthetic_data(true_w, true_b, n_test)
 test_iter = load_array(test_data, batch_size, is_train=False)
 
-# 权重衰减手动实现
-def init_params():
-    """初始化参数"""
-    w = torch.normal(0, 1, size=(num_inputs, 1), requires_grad=True)
-    b = torch.zeros(1, requires_grad=True)
-    return [w, b]
-
-def l2_penalty(w):
-    """L2范数"""
-    return torch.sum(w.pow(2)) / 2
-
-def train(lambd):
-    w, b = init_params()
-    net, loss = lambda X: linreg(X, w, b), squared_loss
+#权重衰减通过pytorch高级api实现
+def train_concise(wd):
+    net = nn.Sequential(nn.Linear(num_inputs, 1))
+    for param in net.parameters():
+        param.data.normal_()
+    loss = nn.MSELoss(reduction='none')
     num_epochs, lr = 100, 0.003
+    # 偏置参数b没有衰减
+    trainer = torch.optim.SGD([
+        {"params":net[0].weight,'weight_decay': wd},
+        {"params":net[0].bias}
+    ], lr=lr)
     animator = Animator(xlabel='epochs', ylabel='loss', yscale='log',
                         xlim=[5, num_epochs], legend=['train', 'test'])
     for epoch in range(num_epochs):
         for X, y in train_iter:
-            # 增加了L2范数惩罚项
-            # 广播机制使得l2_penalty成为一个长度为batch_size的向量
-            l = loss(net(X), y) + lambd * l2_penalty(w)
-            l.sum().backward()
-            sgd([w, b], lr, batch_size)
+            trainer.zero_grad()
+            l = loss(net(X), y)
+            l.mean().backward()
+            trainer.step()
         if (epoch + 1) % 5 == 0:
-            animator.add(epoch + 1, (evaluate_loss(net, train_iter, loss),
-                                     evaluate_loss(net, test_iter, loss)))
-        print(f'weight:{torch.norm(w).item()}')
+            animator.add(epoch + 1,
+                         (evaluate_loss(net, train_iter, loss),
+                         evaluate_loss(net, test_iter, loss)))
+    print('w的L2范数: ', net[0].weight.norm().item())
